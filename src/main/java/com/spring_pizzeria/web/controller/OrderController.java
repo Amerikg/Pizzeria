9e54ced94c6a463184d7a879ab0655acbb37a3d7
Amerikg <amelizruiz@gmail.com>
2025-05-14 12:12:18 -0600

    Add OrderController.java

---
+++ src/main/java/com/spring_pizzeria/web/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.spring_pizzeria.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring_pizzeria.persistence.entity.OrderEntity;
+import com.spring_pizzeria.service.OrderService;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderEntity>> getAll(){
+        return ResponseEntity.ok(this.orderService.getAll());
+    }
+}
