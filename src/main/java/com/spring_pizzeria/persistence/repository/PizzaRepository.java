31f42b9b59b30f4c0c2c3983cd0103d0ecb78503
Amerikg <amelizruiz@gmail.com>
2025-05-19 10:28:26 -0600

    Update PizzaRepository.java

--- src/main/java/com/spring_pizzeria/persistence/repository/PizzaRepository.java
+++ src/main/java/com/spring_pizzeria/persistence/repository/PizzaRepository.java
@@ -1,6 +1,7 @@
 package com.spring_pizzeria.persistence.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.ListCrudRepository;
 
@@ -8,8 +9,9 @@ import com.spring_pizzeria.persistence.entity.PizzaEntity;
 
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
     List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
-    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
+    Optional <PizzaEntity>  findFirstByAvailableTrueAndNameIgnoreCase(String name);
     List<PizzaEntity> findAllByAvailableTrueAndDescriptionContainingIgnoreCase(String description);
-     List<PizzaEntity> findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(String description);
-
+    List<PizzaEntity> findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(String description);
+    List<PizzaEntity> findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(Double price);
+    int countByVeganTrue();
 }
