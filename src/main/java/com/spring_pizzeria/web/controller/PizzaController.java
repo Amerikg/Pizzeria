5014d1a19603d90b14e88dd615afef322d9ade29
Amerikg <amelizruiz@gmail.com>
2025-05-19 10:28:27 -0600

    Update PizzaController.java

--- src/main/java/com/spring_pizzeria/web/controller/PizzaController.java
+++ src/main/java/com/spring_pizzeria/web/controller/PizzaController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import com.spring_pizzeria.persistence.entity.PizzaEntity;
 import com.spring_pizzeria.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,8 +28,9 @@ public class PizzaController {
     }
 
     @GetMapping
-    public ResponseEntity<List<PizzaEntity>> getAll(){
-        return ResponseEntity.ok(this.pizzaService.getAll());
+    public ResponseEntity<Page<PizzaEntity>> getAll(@RequestParam(defaultValue = "0") int page, 
+                                                    @RequestParam(defaultValue = "8") int elements){
+        return ResponseEntity.ok(this.pizzaService.getAll(page, elements));
     }
 
     @GetMapping("/{idPizza}")
@@ -36,8 +39,10 @@ public class PizzaController {
     }
 
     @GetMapping("/available")
-    public ResponseEntity<List<PizzaEntity>> getAvailable(){
-        return ResponseEntity.ok(this.pizzaService.getAvailable());
+    public ResponseEntity<Page<PizzaEntity>> getAvailable(@RequestParam(defaultValue = "0") int page, 
+                                                          @RequestParam(defaultValue = "8") int elements,
+                                                          @RequestParam(defaultValue = "price" ) String sortBy){
+        return ResponseEntity.ok(this.pizzaService.getAvailable(page, elements, sortBy));
     }
 
     @GetMapping("/name/{name}")
@@ -79,5 +84,9 @@ public class PizzaController {
         }
         return ResponseEntity.badRequest().build();
     }
-    
+
+    @GetMapping("/cheapest/{price}")
+    public ResponseEntity<List<PizzaEntity>> getCheapestPizzas(@PathVariable double price){
+        return ResponseEntity.ok(this.pizzaService.getCheapest(price));
+    } 
 }
