505923ffc3882cd8cf5cec06a14a7cd35908851c
Amerikg <amelizruiz@gmail.com>
2025-05-19 15:32:41 -0600

    Update OrderController.java

--- src/main/java/com/spring_pizzeria/web/controller/OrderController.java
+++ src/main/java/com/spring_pizzeria/web/controller/OrderController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,12 @@ public class OrderController {
     public ResponseEntity<List<OrderEntity>> getOutsideOrders(){
         return ResponseEntity.ok(this.orderService.getOutsideOrders());
     }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<List<OrderEntity>> getOutsideOrders(@PathVariable String id){
+        return ResponseEntity.ok(this.orderService.getCustomerOrders(id));
+    }
+
 
 
 
