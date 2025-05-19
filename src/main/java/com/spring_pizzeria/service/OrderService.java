8aa6f44372983504441c76094a1110049e1a03cd
Amerikg <amelizruiz@gmail.com>
2025-05-19 10:28:27 -0600

    Update OrderService.java

--- src/main/java/com/spring_pizzeria/service/OrderService.java
+++ src/main/java/com/spring_pizzeria/service/OrderService.java
@@ -1,5 +1,9 @@
 package com.spring_pizzeria.service;
 
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +16,27 @@ import com.spring_pizzeria.persistence.repository.OrderRepository;
 public class OrderService {
     private final OrderRepository orderRepository;
 
+    private static final String DELIVERY = "D";
+    private static final String CARRYOUT = "C";
+    private static final String ON_SITE = "S";
+
+
     @Autowired
     public OrderService(OrderRepository orderRepository){
         this.orderRepository = orderRepository;
     }
 
     public List<OrderEntity> getAll(){
-        List<OrderEntity> orders = this.orderRepository.findAll();
-        orders.forEach(o -> System.out.println(o.getCustomer().getName()));
-        return orders;
+        return this.orderRepository.findAll();
+    }
+
+    public List<OrderEntity> getTodayOrders(){
+        LocalDateTime today = LocalDate.now().atTime(0,0);
+        return this.orderRepository.findAllByDateAfter(today);
+    }
+
+    public List<OrderEntity> getOutsideOrders(){
+        List<String> methods = Arrays.asList(DELIVERY, CARRYOUT);
+        return this.orderRepository.findAllByMethodIn(methods);
     }
 }
