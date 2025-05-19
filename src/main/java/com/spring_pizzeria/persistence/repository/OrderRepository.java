9c66a51714faf009732755cebd8c794bd2190ccb
Amerikg <amelizruiz@gmail.com>
2025-05-19 15:32:40 -0600

    Update OrderRepository.java

--- src/main/java/com/spring_pizzeria/persistence/repository/OrderRepository.java
+++ src/main/java/com/spring_pizzeria/persistence/repository/OrderRepository.java
@@ -2,7 +2,9 @@ package com.spring_pizzeria.persistence.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.spring_pizzeria.persistence.entity.OrderEntity;
 import java.time.LocalDateTime;
@@ -11,4 +13,7 @@ import java.time.LocalDateTime;
 public interface OrderRepository extends ListCrudRepository<OrderEntity,Integer> {
     List<OrderEntity> findAllByDateAfter(LocalDateTime date);
     List<OrderEntity> findAllByMethodIn(List<String> methods);
+
+    @Query(value = "SELECT * FROM pizza_order WHERE id_customer = :id", nativeQuery = true)
+    List<OrderEntity> findCustomerOrders(@Param("id") String idCustomer);
 }
